495348de1a844602e6600d474cbd02343ada30e4
Tanure97 <tanuretonukari22@gmail.com>
2022-09-10 03:51:40 -0700

    add

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *  main - prints a text
+ *
+ *   Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the out come is a piece of art,\n");
+	return (0);
+}
